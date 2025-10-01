a9161289602df8d6ccb620aca0bef23c5f1acb1b
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-10-01 00:53:32 -0500

    fix: config redis hosta and port

--- src/main/java/com/ecommerce/server/config/RedisConfig.java
+++ src/main/java/com/ecommerce/server/config/RedisConfig.java
@@ -13,10 +13,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host:localhost}")
+    @Value("${spring.data.redis.host:}")
     private String host;
 
-    @Value("${spring.data.redis.port:6379}")
+    @Value("${spring.data.redis.port:}")
     private int port;
 
     @Value("${spring.data.redis.password:}")
