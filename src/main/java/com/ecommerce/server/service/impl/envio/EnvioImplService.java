4579dd03330520990d7e414adcf65eb696163826
Dev-LuzCQDev-LuzCQDev-LuzCQ <dev.luz.cama@gmail.com>
2025-11-02 21:58:28 -0500

    add import

--- src/main/java/com/ecommerce/server/service/impl/envio/EnvioImplService.java
+++ src/main/java/com/ecommerce/server/service/impl/envio/EnvioImplService.java
@@ -1,79 +1,77 @@
-package com.ecommerce.server.service.impl.envio;
+// src/main/java/com/ecommerce/server/service/impl/envio/EnvioImplService.java
+    package com.ecommerce.server.service.impl.envio;
 
-import com.ecommerce.server.model.dao.envio.EnvioDao;
-import com.ecommerce.server.model.dto.envio.EnvioDto;
-import com.ecommerce.server.model.entity.envio.DatosPersonales;
-import com.ecommerce.server.model.entity.envio.Envio;
-import com.ecommerce.server.model.entity.venta.Venta;
-import com.ecommerce.server.service.envio.IDatosPersonalesService;
-import com.ecommerce.server.service.envio.IEnvioService;
-import com.ecommerce.server.service.venta.IVentaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.ArrayList;
+    import com.ecommerce.server.model.dao.envio.EnvioDao;
+    import com.ecommerce.server.model.dto.envio.EnvioDto;
+    import com.ecommerce.server.model.entity.envio.DatosPersonales;
+    import com.ecommerce.server.model.entity.envio.Envio;
+    import com.ecommerce.server.model.entity.venta.Venta;
+    import com.ecommerce.server.service.envio.IDatosPersonalesService;
+    import com.ecommerce.server.service.envio.IEnvioService;
+    import com.ecommerce.server.service.venta.IVentaService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+    import org.springframework.transaction.annotation.Transactional;
+    import java.util.ArrayList;
+    import java.util.List;
 
-import java.util.List;
+    @Service
+    public class EnvioImplService implements IEnvioService {
 
-@Service
-public class EnvioImplService implements IEnvioService {
+        @Autowired
+        private EnvioDao envioDao;
 
-    @Autowired
-    private EnvioDao envioDao;
+        @Autowired
+        private IVentaService ventaService;
 
-    @Autowired
-    private IVentaService ventaService;
-    @Autowired
-    private IDatosPersonalesService datosPersonalesService;
+        @Autowired
+        private IDatosPersonalesService datosPersonalesService;
 
-    @Override
-    public List<Envio> getEnvios() {
-        List<Envio> list = new ArrayList<>();
-        envioDao.findAll().forEach(list::add);
-        return list;
+        @Override
+        public List<Envio> getEnvios() {
+            List<Envio> list = new ArrayList<>();
+            envioDao.findAll().forEach(list::add);
+            return list;
+        }
+
+        @Override
+        public Envio getEnvio(Long id) {
+            return envioDao.findById(id).orElse(null);
+        }
+
+        @Override
+        @Transactional
+        public Envio save(EnvioDto envioDto) {
+            Venta venta = ventaService.getVenta(envioDto.getVentaId());
+            DatosPersonales datosPersonales = datosPersonalesService.getDatoPersonal(envioDto.getDatosPersonalesId());
+
+            Envio envio = Envio.builder()
+                    .id(envioDto.getId())
+                    .venta(venta)
+                    .datosPersonales(datosPersonales)
+                    .costoEnvio(envioDto.getCostoEnvio())
+                    .fechaEnvio(envioDto.getFechaEnvio())
+                    .fechaEntrega(envioDto.getFechaEntrega())
+                    .estado(envioDto.getEstado())
+                    .metodoEnvio(envioDto.getMetodoEnvio())
+                    .trackingNumber(envioDto.getTrackingNumber())
+                    .build();
+
+            return envioDao.save(envio);
+        }
+
+        @Override
+        public void delete(Envio envio) {
+            envioDao.delete(envio);
+        }
+
+        @Override
+        public boolean existsById(Long id) {
+            return envioDao.existsById(id);
+        }
+
+        @Override
+        public List<Envio> obtenerEnviosNoEntregadosPorUsuario(Long userId) {
+            return envioDao.findByUsuarioIdAndEstadoNot(userId, "ENTREGADO");
+        }
     }
-
-    @Override
-    public Envio getEnvio(Long id) {
-        return envioDao.findById(id).orElse(null);
-    }
-
-    @Override
-    @Transactional
-    public Envio save(EnvioDto envioDto) {
-        Venta venta = ventaService.getVenta(envioDto.getVentaId());
-
-        DatosPersonales datosPersonales = datosPersonalesService.getDatoPersonal(envioDto.getDatosPersonalesId());
-
-        Envio envio = Envio.builder()
-                .id(envioDto.getId()) // Si es null, se generará automáticamente
-                .venta(venta)
-                .datosPersonales(datosPersonales)
-                .costoEnvio(envioDto.getCostoEnvio())
-                .fechaEnvio(envioDto.getFechaEnvio())
-                .fechaEntrega(envioDto.getFechaEntrega())
-                .estado(envioDto.getEstado())
-                .metodoEnvio(envioDto.getMetodoEnvio())
-                .trackingNumber(envioDto.getTrackingNumber())
-                .build();
-
-        Envio savedEnvio = envioDao.save(envio);
-
-        return savedEnvio;
-    }
-
-    @Override
-    public void delete(Envio envio) {
-        envioDao.delete(envio);
-    }
-
-    @Override
-    public boolean existsById(Long id) {
-        return envioDao.existsById(id);
-    }
-
-    @Override
-    public List<Envio> obtenerEnviosNoEntregadosPorUsuario(Long userId) {
-        return envioRepository.findByUsuarioIdAndEstadoNot(userId, "ENTREGADO");
-    }
-}
